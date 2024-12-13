ba78581f2a9876ef7c07afbb98ce15524eda2dd5
Lincoln Wallace <lincoln.wallace@canonical.com>
2024-12-13 11:33:28 -0300

    feat(cpu): Add function to check total number of physical cores
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

---
+++ src/cpu/total.go
@@ -0,0 +1,42 @@
+package cpu
+
+import (
+	"encoding/json"
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+type CPUInfo struct {
+	Field    string    `json:"field"`
+	Data     string    `json:"data"`
+	Children []CPUInfo `json:"children"`
+}
+
+func TotalAvailable() (int, error) {
+	cmd := exec.Command("lscpu", "--json")
+	output, err := cmd.Output()
+	if err != nil {
+		return 0, err
+	}
+
+	var result struct {
+		CPUs []CPUInfo `json:"lscpu"`
+	}
+	if err := json.Unmarshal(output, &result); err != nil {
+		return 0, err
+	}
+
+	for _, cpu := range result.CPUs {
+		if strings.TrimSpace(cpu.Field) == "CPU(s):" {
+			totalCPUs, err := strconv.Atoi(cpu.Data)
+			if err != nil {
+				return 0, err
+			}
+			return totalCPUs, nil
+		}
+	}
+
+	return 0, fmt.Errorf("could not find total CPUs")
+}
