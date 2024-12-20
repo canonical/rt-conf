5b8e03e91993c129780cc7d2cce640a49108a814
Lincoln Wallace <lincoln.wallace@canonical.com>
2024-12-20 06:11:42 -0300

    many: changes
      * add: info msg to Model struct
      * add: error msg to the model struct
      * feat: Use Init() method to initialize a main menu
      * refact: Move main menu logic update to updateMainMenu()
      * fix: update bug of keys being registered to kcmdline menu
        when on main menu by add if statement to check current menu
        on updateInputs()
      * feat: Enable spinner toggle
      * feat: separate logic to handle index overflow to separate funcs
        NextIndex() and PrevIndex()
      * add: Execution logic to call internal functions and update
        bootloader
    
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- src/ui/model.go
+++ src/ui/model.go
@@ -2,8 +2,10 @@ package ui
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/canonical/rt-conf/src/data"
+	"github.com/canonical/rt-conf/src/kcmd"
 	"github.com/charmbracelet/bubbles/cursor"
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
@@ -28,9 +30,9 @@ func (i item) FilterValue() string { return i.title }
 type listKeyMap struct {
 	// toggleTitleBar   key.Binding
 	// insertItem       key.Binding
-	goHome     key.Binding
-	selectMenu key.Binding
-	// toggleSpinner    key.Binding
+	goHome        key.Binding
+	selectMenu    key.Binding
+	toggleSpinner key.Binding
 	// togglePagination key.Binding
 	toggleHelpMenu key.Binding
 }
@@ -53,10 +55,10 @@ func newListKeyMap() *listKeyMap {
 			key.WithKeys("enter", "space"),
 			key.WithHelp("enter", "select menu"),
 		),
-		// toggleSpinner: key.NewBinding(
-		// 	key.WithKeys("s"),
-		// 	key.WithHelp("s", "toggle spinner"),
-		// ),
+		toggleSpinner: key.NewBinding(
+			key.WithKeys("s"),
+			key.WithHelp("s", "toggle spinner"),
+		),
 		// togglePagination: key.NewBinding(
 		// 	key.WithKeys("P"),
 		// 	key.WithHelp("P", "toggle pagination"),
@@ -116,20 +118,21 @@ type Model struct {
 	prevMenu      menuOpt
 	currMenu      menuOpt
 	errorMsg      string
+	infoMsg       string
 }
 
 func NewModel(c *data.InternalConfig) Model {
 	var (
-		itemGenerator menuItems
-		delegateKeys  = newDelegateKeyMap()
-		listKeys      = newListKeyMap()
+		menuOpts     menuItems
+		delegateKeys = newDelegateKeyMap()
+		listKeys     = newListKeyMap()
 	)
 
+	menuOpts.Init()
 	// Make initial list of items
-	const numItems = 2
-	items := make([]list.Item, numItems)
-	for i := 0; i < numItems; i++ {
-		items[i] = itemGenerator.next()
+	items := make([]list.Item, menuOpts.Size())
+	for i := 0; i < menuOpts.Size(); i++ {
+		items[i] = menuOpts.next()
 	}
 
 	// Setup list
@@ -142,7 +145,7 @@ func NewModel(c *data.InternalConfig) Model {
 	menuList.AdditionalFullHelpKeys = func() []key.Binding {
 
 		return []key.Binding{
-			// listKeys.toggleSpinner,
+			listKeys.toggleSpinner,
 			// listKeys.insertItem,
 			// listKeys.toggleTitleBar,
 			// listKeys.togglePagination,
@@ -152,18 +155,23 @@ func NewModel(c *data.InternalConfig) Model {
 	}
 
 	return Model{
+		// TODO: Fix this info msg, put in a better place
+		infoMsg:       "Please fill all fields before submit\n",
 		inputs:        newTextInputs(),
 		iconf:         *c,
 		list:          menuList,
 		keys:          listKeys,
 		delegateKeys:  delegateKeys,
-		itemGenerator: &itemGenerator,
+		itemGenerator: &menuOpts,
 	}
 }
 
 func (m Model) Init() tea.Cmd {
 	return textinput.Blink
 }
+
+// TODO: FOCUS ON THE [ APPLY ] FUNCTIONALITY
+// TODO:
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmds []tea.Cmd
@@ -180,66 +188,17 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case tea.KeyMsg:
 		// Don't match any of the keys below if we're actively filtering.
-		if m.list.FilterState() == list.Filtering {
-			break
-		}
 
-		if m.currMenu == mainMenu {
-			switch {
-			// case key.Matches(msg, m.keys.toggleSpinner):
-			// 	cmd := m.list.ToggleSpinner()
-			// 	return m, cmd
+		if m.currMenu != mainMenu {
 
-			case key.Matches(msg, m.keys.goHome):
-				m.currMenu = mainMenu
-
-			case key.Matches(msg, m.keys.selectMenu):
-				selected := m.list.SelectedItem().(item)
-
-				// TODO: Improve this selection logic
-				// It could be indexed by the menu item
-				switch selected.Title() {
-				case "Kernel cmdline":
-					m.prevMenu = mainMenu
-					m.currMenu = kcmdlineMenu
-				case "IRQ Affinity":
-					m.prevMenu = mainMenu
-					m.currMenu = irqAffinityMenu
-				}
-				return m, nil
-			// case key.Matches(msg, m.keys.toggleTitleBar):
-			// 	v := !m.list.ShowTitle()
-			// 	m.list.SetShowTitle(v)
-			// 	m.list.SetShowFilter(v)
-			// 	m.list.SetFilteringEnabled(v)
-			// 	return m, nil
-
-			// case key.Matches(msg, m.keys.togglePagination):
-			// 	m.list.SetShowPagination(!m.list.ShowPagination())
-			// 	return m, nil
-
-			case key.Matches(msg, m.keys.toggleHelpMenu):
-				m.list.SetShowHelp(!m.list.ShowHelp())
-				return m, nil
-
-				// case key.Matches(msg, m.keys.insertItem):
-				// 	m.delegateKeys.choose.SetEnabled(true)
-				// 	newItem := m.itemGenerator.next()
-				// 	insCmd := m.list.InsertItem(0, newItem)
-				// 	statusCmd := m.list.NewStatusMessage(statusMessageStyle("Added " + newItem.Title()))
-				// 	return m, tea.Batch(insCmd, statusCmd)
-			}
-		} else {
-
-			// if m.currMenu == kcmdlineMenu && m.prevMenu == mainMenu {
-			// 	// Cleanup the inputs
-			// 	log.Println("Cleaning up inputs")
-			// 	m.inputs = nil
-			// }
-
+			// TODO: Move this logic to a func specific for kcmdlineMenu
 			switch msg.String() {
 			case "ctrl+c", "esc":
 				return m, tea.Quit
+
+			case "g":
+				m.currMenu = mainMenu
+				return m, nil
 
 			// Change cursor mode
 			case "ctrl+r":
@@ -257,29 +216,56 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			case "tab", "shift+tab", "enter", "up", "down":
 				s := msg.String()
 
+				log.Println("focusIndex on Update: ", m.focusIndex)
+
+				isValid := m.Validation()
+				log.Println("isValid: ", isValid)
 				// Did the user press enter while the submit button was focused?
 				// If so, exit.
 				if s == "enter" && m.focusIndex == len(m.inputs) {
-					return m, tea.Quit
+					log.Println("Apply changes")
+
+					valid := m.Validation()
+
+					dyntickIdle, err := strconv.ParseBool(m.inputs[enableDynticks].Value())
+					if err != nil {
+						log.Printf("Failed to parse dyntick idle value: %v", err)
+						break
+					}
+					if !valid {
+						break
+					}
+					// TODO: fix this
+					m.iconf.Data.KernelCmdline.DyntickIdle = dyntickIdle
+					m.iconf.Data.KernelCmdline.IsolCPUs = m.inputs[isolatecpus].Value()
+					m.iconf.Data.KernelCmdline.AdaptiveTicks = m.inputs[enableDynticks].Value()
+
+					// IsolCPUs      string `yaml:"isolcpus"`       // Isolate CPUs
+					// DyntickIdle   bool   `yaml:"dyntick-idle"`   // Enable/Disable dyntick idle
+					// AdaptiveTicks string `yaml:"adaptive-ticks"` // CPUs for adaptive ticks
+
+					kcmd.ProcessKcmdArgs(&m.iconf)
+					// TODO: Apply the changes call the kcmdline funcs
+					// TODO: Present a new view (maybe a new block with text)
 				}
 
 				log.Println("Focus index is: ", m.focusIndex)
-				log.Println("Value: ", m.inputs[m.focusIndex].Value())
-
-				m.Validation() //TODO: VALIDATION HAPPENS HERE
+				if m.focusIndex < len(m.inputs) {
+					log.Println("Value: ", m.inputs[m.focusIndex].Value())
+				}
 
 				// Cycle indexes
 				if s == "up" || s == "shift+tab" {
-					m.focusIndex--
+					m.PrevIndex()
 				} else {
-					m.focusIndex++
+					m.NextIndex()
 				}
 
-				if m.focusIndex > len(m.inputs) {
-					m.focusIndex = 0
-				} else if m.focusIndex < 0 {
-					m.focusIndex = len(m.inputs)
-				}
+				// if m.focusIndex > len(m.inputs) {
+				// 	m.focusIndex = 0
+				// } else if m.focusIndex < 0 {
+				// 	m.focusIndex = len(m.inputs)
+				// }
 
 				cmds := make([]tea.Cmd, len(m.inputs))
 				for i := 0; i <= len(m.inputs)-1; i++ {
@@ -298,28 +284,99 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 				return m, tea.Batch(cmds...)
 			}
-			cmdInput = m.updateInputs(msg)
 		}
 	}
 
+	cmdMainMenu := m.updateMainMenu(msg)
+	cmdInput = m.updateInputs(msg)
 	// This will also call our delegate's update function.
 	newListModel, cmd := m.list.Update(msg)
 	m.list = newListModel
 	cmds = append(cmds, cmd)
 	cmds = append(cmds, cmdInput)
+	cmds = append(cmds, cmdMainMenu)
 
 	return m, tea.Batch(cmds...)
+}
+
+func (m *Model) NextIndex() {
+	m.focusIndex++
+	if m.focusIndex > len(m.inputs) {
+		m.focusIndex = 0
+	}
+}
+
+func (m *Model) PrevIndex() {
+	m.focusIndex--
+	if m.focusIndex == -1 {
+		m.focusIndex = len(m.inputs)
+	}
+}
+
+// func (m *Model) updateKcmdlineMenu(msg tea.Msg) tea.Cmd {
+// 	cmds := make([]tea.Cmd, len(m.inputs))
+// 	switch msg := msg.(type) {
+// 	case tea.KeyMsg:
+// 		switch {
+// 		case key.Matches(msg, m.keys.goHome):
+// 			m.currMenu = mainMenu
+// 		}
+// 	return tea.Batch(cmds...)
+// }
+
+func (m *Model) updateMainMenu(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
+
+	if m.currMenu == mainMenu {
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+
+			if m.list.FilterState() == list.Filtering {
+				break
+			}
+			switch {
+			case key.Matches(msg, m.keys.toggleSpinner):
+				cmd := m.list.ToggleSpinner()
+				cmds = append(cmds, cmd)
+
+			case key.Matches(msg, m.keys.goHome):
+				m.currMenu = mainMenu
+
+			case key.Matches(msg, m.keys.selectMenu):
+				selected := m.list.SelectedItem().(item)
+
+				// TODO: Improve this selection logic
+				// It could be indexed by the menu item
+				switch selected.Title() {
+				case "Kernel cmdline":
+					m.prevMenu = mainMenu
+					m.currMenu = kcmdlineMenu
+				case "IRQ Affinity":
+					m.prevMenu = mainMenu
+					m.currMenu = irqAffinityMenu
+				}
+
+			case key.Matches(msg, m.keys.toggleHelpMenu):
+				m.list.SetShowHelp(!m.list.ShowHelp())
+				// return m, nil
+			}
+
+		}
+
+	}
+
+	return tea.Batch(cmds...)
 }
 
 func (m *Model) updateInputs(msg tea.Msg) tea.Cmd {
 	cmds := make([]tea.Cmd, len(m.inputs))
 
-	// Only text inputs with Focus() set will respond, so it's safe to simply
-	// update all of them here without any further logic.
-	for i := range m.inputs {
-		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
-
-		// log.Println("Value is: ", m.inputs[i].Value())
+	if m.currMenu == kcmdlineMenu {
+		// Only text inputs with Focus() set will respond, so it's safe to simply
+		// update all of them here without any further logic.
+		for i := range m.inputs {
+			m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+		}
 	}
 
 	return tea.Batch(cmds...)
