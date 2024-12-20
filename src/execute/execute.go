c2d920ab961aff13f4fd30794c824144edf600be
Lincoln Wallace <lincoln.wallace@canonical.com>
2024-12-20 08:21:10 -0300

    feat: add validation to ensure equal number of lines
      Avoid to break the TUI rendering by ensure no matter
      what is the system, the execution instruction for
      kcmd will be the same size on lines
    
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- src/execute/execute.go
+++ src/execute/execute.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 )
 
-// TODO: Add a validation for the number of lines in each execute message
 // NOTE: THESE MESSAGES MUST ALWAYS BE THE SAME NUMBER OF LINES SO IT DOESN'T
 // BROKE THE TUI
+
+const lineNums = 6
 
 // NOTE: These messages should be serialized into a struct/json for future use of TUI app
 func GrubConclusion() []string {
@@ -18,17 +19,27 @@ func GrubConclusion() []string {
 		"\n",
 		"to apply the changes\n",
 	}
+	if len(s) != lineNums {
+		panic("GrubConclusion: invalid number of lines")
+	}
 	return s
 }
 
 func RpiConclusion(cmdline []string) []string {
 	s := []string{
-		"Please, append the following to /boot/firmware/cmdline.txt:\n",
-		"In case of old style boot partition,\n",
-		"append to /boot/cmdline.txt\n\n",
+		"Please, append the following to /boot/firmware/cmdline.txt:\n", // 1
+		"In case of old style boot partition,\n",                        // 2
+		"append to /boot/cmdline.txt\n",                                 // 3
+		"\n",                                                            // 4
 	}
+	kcmdline := ""
 	for _, param := range cmdline {
-		s = append(s, fmt.Sprintf("%s ", param))
+		kcmdline += fmt.Sprintf("%s ", param)
+	}
+	s = append(s, fmt.Sprintf("%s\n", kcmdline)) // 5
+	s = append(s, "\n")                          // 6
+	if len(s) != lineNums {
+		panic("RpiConclusion: invalid number of lines")
 	}
 	return s
 }
