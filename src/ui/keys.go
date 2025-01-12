f68a28d4e1b212efcc63dc7933d2f95669f6dca2
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-01-12 17:12:52 -0300

    feat: fix help view
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- src/ui/keys.go
+++ src/ui/keys.go
@@ -6,18 +6,23 @@ type listKeyMap struct {
 	Up         key.Binding
 	Down       key.Binding
 	goHome     key.Binding
-	selectMenu key.Binding
+	Select     key.Binding
 	Help       key.Binding
 	Quit       key.Binding
+	CursorMode key.Binding
 }
 
+// FullHelp returns keybindings for the expanded help view. It's part of the
+// key.Map interface.
 func (k listKeyMap) FullHelp() [][]key.Binding {
 	return [][]key.Binding{
-		{k.Up, k.Down}, // first column
-		{k.Help},       // second column
+		{k.Up, k.Down, k.goHome},   // first column
+		{k.Help, k.Select, k.Quit}, // second column
 	}
 }
 
+// ShortHelp returns keybindings to be shown in the mini help view. It's part
+// of the key.Map interface.
 func (k listKeyMap) ShortHelp() []key.Binding {
 	return []key.Binding{k.Help, k.Quit}
 }
@@ -32,17 +37,31 @@ func newListKeyMap() *listKeyMap {
 		// 	key.WithKeys("T"),
 		// 	key.WithHelp("T", "toggle title"),
 		// ),
+
+		CursorMode: key.NewBinding(
+			key.WithKeys("ctrl+r"),
+			key.WithHelp("ctrl+r", "toggle cursor mode"),
+		),
+
+		Up: key.NewBinding(
+			key.WithKeys("up", "k"),
+			key.WithHelp("↑/k", "move up"),
+		),
+		Down: key.NewBinding(
+			key.WithKeys("down", "j"),
+			key.WithHelp("↓/j", "move down"),
+		),
 		goHome: key.NewBinding(
 			key.WithKeys("g", "home"),
 			key.WithHelp("g", "Home screen"),
 		),
-		selectMenu: key.NewBinding(
+		Select: key.NewBinding(
 			key.WithKeys("enter", "space"),
 			key.WithHelp("enter", "select menu"),
 		),
 		Quit: key.NewBinding(
 			key.WithKeys("q", "ctl+c"),
-			key.WithHelp("q", "quit"),
+			key.WithHelp("q/ctrl+c", "quit"),
 		),
 		// toggleSpinner: key.NewBinding(
 		// 	key.WithKeys("s"),
