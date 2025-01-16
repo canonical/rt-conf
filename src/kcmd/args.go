0302ad2aa646b7e06979dcf8efe4d2e42a63dc7a
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-01-16 09:39:04 -0300

    refact: change variable name
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- src/kcmd/args.go
+++ src/kcmd/args.go
@@ -21,11 +21,11 @@ func ProcessKcmdArgs(c *data.InternalConfig) ([]string, error) {
 		msgs = append(msgs, tmp...)
 	case system.Grub:
 		msgs = append(msgs, "Detected bootloader: GRUB\n")
-		tmp, err := models.UpdateGrub(c)
+		msg, err := models.UpdateGrub(c)
 		if err != nil {
 			return nil, fmt.Errorf("failed to read file: %v", err)
 		}
-		msgs = append(msgs, tmp...)
+		msgs = append(msgs, msg...)
 	default:
 		return nil, fmt.Errorf("unsupported bootloader: %v", sys)
 	}
