22efa6225c1ebc413d5bf02147555740b86aa33f
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-02-10 01:01:37 -0300

    feat: add custom message structs to be sended as tea.Msg
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- src/ui/irq.go
+++ src/ui/irq.go
@@ -1,30 +1,34 @@
 package ui
 
 import (
-	"log"
-
-	"github.com/canonical/rt-conf/src/ui/styles"
-	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/list"
-	tea "github.com/charmbracelet/bubbletea"
+	"github.com/canonical/rt-conf/src/data"
 )
 
 // TODO: add conclussion screen saying:
 // "5 IRQ filter rules are aplied to the system"
 // [ BACK ]
 
-type NewIRQEntryMsg struct {
-	irqAffinityRule
+type IRQRuleMsg struct {
+	IRQAffinityRule
 }
 
-type irqAffinityRule struct {
+type IRQAffinityRule struct {
+	edited          bool // false means it's a new rule, true means new rule
+	index           int
+	rule            data.IRQTunning
 	filter, cpulist string
 }
 
-func (i irqAffinityRule) Title() string       { return i.filter }
-func (i irqAffinityRule) Description() string { return i.cpulist }
+type StartNewIRQAffinityRule struct {
+	editMode bool
+	// ** If editMode is true, this is the index of the rule
+	// **  to edit as well the cpu list and filter
+	index           int
+	filter, cpulist string
+}
+
+func (i IRQAffinityRule) Title() string       { return i.filter }
+func (i IRQAffinityRule) Description() string { return i.cpulist }
 
 // This needs to be implemented to satisfy the list.Item interface
-func (i irqAffinityRule) FilterValue() string { return i.filter }
-
-
+func (i IRQAffinityRule) FilterValue() string { return i.filter }
