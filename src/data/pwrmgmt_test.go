06aab5c3880e5f79be3592bc7715e54fd2c8df97
Lincoln Wallace <locnnil@hotmail.com>
2025-02-25 09:18:00 -0300

    refact: add proper types on printing

--- src/data/pwrmgmt_test.go
+++ src/data/pwrmgmt_test.go
@@ -61,7 +61,7 @@ func TestPwrMgmtValidationUnhappy(t *testing.T) {
 				t.Fatalf("Expected error on test #%v: %v", i, tc.sclgov)
 			}
 			if err.Error() != tc.err {
-				t.Fatalf("Expected error message: %v, got: %v", tc.err, err)
+				t.Fatalf("Expected error message: %s, got: %s", tc.err, err)
 			}
 
 		})
