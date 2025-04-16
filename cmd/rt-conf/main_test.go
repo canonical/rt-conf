a5a78deb4f539c20689e201a2b985ee960c3009b
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-04-16 19:45:10 -0300

    feat: increase cmd/rt-conf coverage to 89.5%
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- cmd/rt-conf/main_test.go
+++ cmd/rt-conf/main_test.go
@@ -1,30 +1,89 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 )
 
-func TestRunNoConfigPath(t *testing.T) {
-	err := run([]string{})
-	if err == nil {
-		t.Fatalf("expected error, got nil")
+func TestRunUnhappy(t *testing.T) {
+	type tests struct {
+		name string // test name
+		args []string
+		err  string
+		yaml string
 	}
-	if !strings.Contains(err.Error(), "path not set") {
-		t.Fatalf("expected 'failed to load config file', got: %v", err)
-	}
-}
 
-func TestRunFlagParseError(t *testing.T) {
-	err := run([]string{"-bogus"})
-	if err == nil || !strings.Contains(err.Error(), "failed to parse flags") {
-		t.Fatalf("expected flag parse error, got %v", err)
-	}
-}
+	tmpdir := t.TempDir()
+	configPath := filepath.Join(tmpdir, "config.yaml")
 
-func TestRunConfigLoadError(t *testing.T) {
-	err := run([]string{"-file", "/does/not/exist"})
-	if err == nil || !strings.Contains(err.Error(), "failed to find file") {
-		t.Fatalf("expected config load error, got %v", err)
+	var testCases = []tests{
+		{
+			name: "No config path",
+			args: []string{"rt-conf"},
+			err:  "path not set",
+			yaml: "",
+		},
+		{
+			name: "Invalid config path",
+			args: []string{"rt-conf", "-file", "/does/not/exist"},
+			err:  "failed to find file",
+			yaml: "",
+		},
+		{
+			name: "Invalid config + verbose mode",
+			args: []string{"rt-conf", "--verbose", "-file", "/does/not/exist"},
+			err:  "failed to find file",
+			yaml: "",
+		},
+		{
+			name: "Error processing kernel cmdline args",
+			args: []string{"rt-conf", "-file", configPath},
+			err:  "failed to process kernel cmdline args",
+			yaml: `
+kernel_cmdline:
+    nohz: "on"
+`,
+		},
+		{
+			name: "No IRQs found",
+			args: []string{"rt-conf", "-file", configPath},
+			err:  "failed to process interrupts",
+			yaml: `
+irq_tuning:
+    - cpus: "0"
+      filter:
+          actions: "xxxxxx"
+`,
+		},
+		{
+			name: "Failed to process power management config",
+			args: []string{"rt-conf", "-file", configPath},
+			err:  "failed to process power management config",
+			yaml: `
+cpu_governance:
+    - cpus: "0"
+      scaling_governor: "performance"
+`,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			if test.yaml != "" {
+				if err := os.WriteFile(configPath,
+					[]byte(test.yaml), 0644); err != nil {
+					t.Fatalf("failed to write file: %v", err)
+				}
+			}
+			err := run(test.args)
+			if err == nil {
+				t.Fatalf("expected error, got nil")
+			}
+			if !strings.Contains(err.Error(), test.err) {
+				t.Fatalf("expected error '%s', got: %v", test.err, err)
+			}
+		})
 	}
 }
