142e45affa6cb82beda1e8c1e27bba624d9ce8be
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-04-18 16:55:55 -0300

    feat: make ProcessFile mockable
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- src/model/file.go
+++ src/model/file.go
@@ -52,3 +52,50 @@ func ReadYAML(path string) (cfg *Config, err error) {
 
 	return cfg, nil
 }
+<<<<<<< HEAD
+=======
+
+// processFile processes a file with a given FileTransformer, applying
+// its transformation on lines matching the pattern.
+var ProcessFile = func(transformer FileTransformer) error {
+	// Open file with read and write permissions
+	file, err := os.OpenFile(transformer.GetFilePath(), os.O_RDWR, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	// Read all lines into a slice
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if transformer.GetPattern().MatchString(line) {
+			// This is where the kcmdline params of bootloader file are updated
+			line = transformer.TransformLine(line)
+		}
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("failed to read file: %v", err)
+	}
+
+	// Truncate file and write transformed lines
+	if err := file.Truncate(0); err != nil {
+		return fmt.Errorf("failed to truncate file: %v", err)
+	}
+	if _, err := file.Seek(0, 0); err != nil {
+		return fmt.Errorf("failed to seek to start of file: %v", err)
+	}
+
+	for _, line := range lines {
+		_, err := file.WriteString(line + "\n")
+		if err != nil {
+			return fmt.Errorf("failed to write to file: %v", err)
+		}
+	}
+
+	return nil
+}
+>>>>>>> 0729ce3 (feat: make ProcessFile mockable)
