1a4121a9f61fa76dc4e9ce05fc5e44c5b9640501
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-04-24 23:44:32 -0300

    feat: change impl using interface to function pointer
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- src/irq/interface.go
+++ src/irq/interface.go
@@ -1,24 +1,9 @@
 package irq
 
-import (
-	"os"
-)
-
 // IRQReaderWriter is an interface for read and write IRQ data from the filesystem.
 type IRQReaderWriter interface {
 	ReadIRQs() ([]IRQInfo, error)
 	WriteCPUAffinity(irqNum int, cpus string) error
-}
-
-// Abstract the write operation
-type FileWriter interface {
-	WriteFile(path string, content []byte, perm os.FileMode) error
-}
-
-type realFileWriter struct{}
-
-func (realFileWriter) WriteFile(path string, content []byte, perm os.FileMode) error {
-	return os.WriteFile(path, content, perm)
 }
 
 // IRQInfo represents information about an IRQ.
