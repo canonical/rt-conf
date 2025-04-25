1d2b4ba77a92b3b9fba51f576dd65b4825bf7903
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-04-25 14:23:03 -0300

    feat: add config file integrity checks (#55)
    * Check if config file is owned by Root
    * Check if the config file is only writable by Root
    * Add tests
    
    ---------
    
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- src/model/config.go
+++ src/model/config.go
@@ -3,12 +3,34 @@ package model
 import (
 	"fmt"
 	"os"
+	"syscall"
+	"testing"
 )
 
+var expectedPermission os.FileMode = 0o644
+
+var isOwnedByRoot = func(fi os.FileInfo) bool {
+	if testing.Testing() {
+		return true // Always return true in test mode
+	}
+	uid := fi.Sys().(*syscall.Stat_t).Uid
+	return uid == 0 // Check if the file is owned by root
+}
+
 func LoadConfigFile(confPath string) (*Config, error) {
-	_, err := os.Stat(confPath)
+	fileInfo, err := os.Stat(confPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to find file: %v", err)
+	}
+
+	if fileInfo.Mode() != expectedPermission {
+		return nil, fmt.Errorf(
+			"file %s has invalid permissions: %v, expected permissions %v",
+			confPath, fileInfo.Mode(), expectedPermission)
+	}
+
+	if !isOwnedByRoot(fileInfo) {
+		return nil, fmt.Errorf("file %s is not owned by root", confPath)
 	}
 
 	content, err := ReadYAML(confPath)
---
+++ src/model/config_test.go
@@ -0,0 +1,133 @@
+package model
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestIsOwnedByRoot(t *testing.T) {
+	tmpdir := t.TempDir()
+	filePath := filepath.Join(tmpdir, "testfile")
+	err := os.WriteFile(filePath,
+		[]byte("test"), 0644)
+	if err != nil {
+		t.Fatalf("failed to create test file: %v", err)
+	}
+	fi, err := os.Stat(filePath)
+	if err != nil {
+		t.Fatalf("failed to stat test file: %v", err)
+	}
+	if !isOwnedByRoot(fi) {
+		t.Fatalf("expected true, got false")
+	}
+
+}
+
+func TestLoadConfigFile(t *testing.T) {
+	var testCases = []struct {
+		name        string
+		yaml        string
+		perm        os.FileMode
+		cfg         *Config
+		err         error
+		ownedByRoot bool
+	}{
+		{
+			name:        "FileDoesNotExist",
+			yaml:        "",
+			perm:        0644,
+			cfg:         nil,
+			err:         fmt.Errorf("failed to find file"),
+			ownedByRoot: true,
+		},
+		{
+			name:        "FileInvalidPermissions",
+			yaml:        "kernel_cmdline:",
+			perm:        0755,
+			cfg:         nil,
+			err:         fmt.Errorf("has invalid permissions"),
+			ownedByRoot: true,
+		},
+		{
+			name:        "FileNotOwnedByRoot",
+			yaml:        `kernel_cmdline:`,
+			perm:        0644,
+			cfg:         nil,
+			err:         fmt.Errorf("not owned by root"),
+			ownedByRoot: false,
+		},
+		{
+			name:        "FailedToUnmarshalYAML",
+			yaml:        `kernel_cmdline: {`,
+			perm:        0644,
+			cfg:         nil,
+			err:         fmt.Errorf("failed to unmarshal data"),
+			ownedByRoot: true,
+		},
+		{
+			name: "FileValid",
+			yaml: `
+kernel_cmdline:
+    nohz: "on"
+    isolcpus: "1"
+    kthread_cpus: "0"
+    irqaffinity: "0"
+`,
+			perm: 0644,
+			cfg: &Config{
+				KernelCmdline: KernelCmdline{
+					Nohz:        "on",
+					IsolCPUs:    "1",
+					KthreadCPUs: "0",
+					IRQaffinity: "0",
+				},
+			},
+			ownedByRoot: true,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tmpdir := t.TempDir()
+			cfgFilePath := filepath.Join(tmpdir, "config.yaml")
+
+			isOwnedByRoot = func(_ os.FileInfo) bool {
+				return tc.ownedByRoot
+			}
+
+			if tc.yaml != "" {
+				if err := os.WriteFile(cfgFilePath,
+					[]byte(tc.yaml), tc.perm); err != nil {
+					t.Fatalf("failed to write file: %v", err)
+				}
+			}
+
+			cfg, err := LoadConfigFile(cfgFilePath)
+
+			// Unhappy cases
+			if tc.err != nil {
+				if err == nil {
+					t.Fatalf("expected error containing %q, got nil", tc.err)
+				}
+				if !strings.Contains(err.Error(), tc.err.Error()) {
+					t.Fatalf("expected error to contain %q, got: %v", tc.err, err)
+				}
+				return
+			}
+
+			// Happy cases
+			if err != nil {
+				t.Fatalf("expected no error, got '%v'", err)
+			}
+			if cfg == nil {
+				t.Fatalf("expected non-nil config, got nil")
+			}
+			if !reflect.DeepEqual(cfg, tc.cfg) {
+				t.Fatalf("expected config %v, got %v", tc.cfg, cfg)
+			}
+		})
+	}
+}
