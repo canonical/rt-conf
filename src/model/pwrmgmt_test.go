fa6f09b4cc12e194f056f7131ee7d5dbfd17984d
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-07-01 11:57:15 -0300

    test(model/pwrmgmt): update tests
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- src/model/pwrmgmt_test.go
+++ src/model/pwrmgmt_test.go
@@ -68,7 +68,7 @@ func TestPwrMgmtValidationUnhappy(t *testing.T) {
 	}
 }
 
-func TestCheckFreqFormat(t *testing.T) {
+func TestCheckFreqFormat(t *testing.T) { //TODO: drop this test
 	exFormat := "expected formats: 3.4GHz, 2000MHz, 100000KHz, got: "
 	tests := []struct {
 		name    string
@@ -78,6 +78,7 @@ func TestCheckFreqFormat(t *testing.T) {
 		{
 			name: "valid GHz and MHz",
 			rule: CpuGovernanceRule{
+				CPUs:    "0",
 				MinFreq: "1.8GHz",
 				MaxFreq: "3.4GHz",
 			},
@@ -86,80 +87,88 @@ func TestCheckFreqFormat(t *testing.T) {
 		{
 			name: "invalid unit only value",
 			rule: CpuGovernanceRule{
+				CPUs:    "0",
 				MinFreq: "MHz",
 				MaxFreq: "GHz",
 			},
-			wantErr: "invalid min frequency: invalid frequency format: " +
+			wantErr: "invalid min_freq: invalid frequency format: " +
 				exFormat + "MHz",
 		},
 		{
 			name: "invalid G and M suffix only",
 			rule: CpuGovernanceRule{
+				CPUs:    "0",
 				MinFreq: "1.2M",
 				MaxFreq: "2.4G",
 			},
-			wantErr: "invalid min frequency: invalid frequency format: " +
+			wantErr: "invalid min_freq: invalid frequency format: " +
 				exFormat + "1.2M",
 		},
 		{
 			name: "invalid no unit suffix",
 			rule: CpuGovernanceRule{
+				CPUs:    "0",
 				MinFreq: "1800000",
 				MaxFreq: "2400000",
 			},
-			wantErr: "invalid min frequency: invalid frequency format: " +
+			wantErr: "invalid min_freq: invalid frequency format: " +
 				exFormat + "1800000",
 		},
 		{
 			name: "invalid float without unit",
 			rule: CpuGovernanceRule{
+				CPUs:    "0",
 				MinFreq: "1.2",
 				MaxFreq: "2.5",
 			},
-			wantErr: "invalid min frequency: invalid frequency format: " +
+			wantErr: "invalid min_freq: invalid frequency format: " +
 				exFormat + "1.2",
 		},
 		{
 			name: "valid lowercase hz",
 			rule: CpuGovernanceRule{
+				CPUs:    "0",
 				MinFreq: "1.0mhz",
 				MaxFreq: "3.0gghz",
 			},
-			wantErr: "invalid max frequency: invalid frequency format: " +
+			wantErr: "invalid max_freq: invalid frequency format: " +
 				exFormat + "3.0gghz",
 		},
 		{
 			name: "invalid max freq string",
 			rule: CpuGovernanceRule{
+				CPUs:    "0",
 				MinFreq: "1.2GHz",
 				MaxFreq: "threeGHz",
 			},
-			wantErr: "invalid max frequency: invalid frequency format: " +
+			wantErr: "invalid max_freq: invalid frequency format: " +
 				exFormat + "threeGHz",
 		},
 		{
 			name: "invalid min freq string",
 			rule: CpuGovernanceRule{
+				CPUs:    "0",
 				MinFreq: "oneGHz",
 				MaxFreq: "3.4GHz",
 			},
-			wantErr: "invalid min frequency: invalid frequency format: " +
+			wantErr: "invalid min_freq: invalid frequency format: " +
 				exFormat + "oneGHz",
 		},
 		{
 			name: "both max and min invalid",
 			rule: CpuGovernanceRule{
+				CPUs:    "0",
 				MinFreq: "badMin",
 				MaxFreq: "badMax",
 			},
-			wantErr: "invalid min frequency: invalid frequency format: " +
+			wantErr: "invalid min_freq: invalid frequency format: " +
 				exFormat + "badMin",
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			err := tc.rule.CheckFreqFormat()
+			err := tc.rule.Validate()
 			if tc.wantErr == "" && err != nil {
 				t.Fatalf("expected no error, got: %v", err)
 			}
