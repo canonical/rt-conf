7eb34b03bc24a338c75c17d624a7fd400c383918
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-08-22 08:27:52 -0300

    feat: drop automatic linter changes
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- src/irq/irq.go
+++ src/irq/irq.go
@@ -44,7 +44,7 @@ type IRQs map[int]bool // use the same logic as CPUs lists
 // realIRQReaderWriter writes CPU affinity to the real `/proc/irq/<irq>/smp_affinity_list` file.
 type realIRQReaderWriter struct{}
 
-var procIRQ      = model.ProcIRQ
+var procIRQ = model.ProcIRQ
 var sysKernelIRQ = model.SysKernelIRQ
 
 var writeFile = func(path string, content []byte, perm os.FileMode) error {
